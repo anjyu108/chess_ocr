a884ef906720b20d82aeac12d70bb961aff4c41f
Naoki.Yasuno <Naoki.Yasuno@sony.com>
2023-11-11 18:34:02 +0900

    Comment-in download of the IAM dataset

--- ocr_feasibility_study/get_dataset.sh
+++ ocr_feasibility_study/get_dataset.sh
@@ -1,7 +1,7 @@
 set -aux
 
-# wget -q https://github.com/sayakpaul/Handwriting-Recognizer-in-Keras/releases/download/v1.0.0/IAM_Words.zip
-# unzip -qq IAM_Words.zip
+wget -q https://github.com/sayakpaul/Handwriting-Recognizer-in-Keras/releases/download/v1.0.0/IAM_Words.zip
+unzip -qq IAM_Words.zip
 
 mkdir data
 mkdir data/words
